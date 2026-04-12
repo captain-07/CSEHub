68279c5802ee0fa91088318940bfb0632f5cd14a
Debjyoti <sahadebjyoti363@gmail.com>
2026-04-12 21:50:30 +0530

    Add admin configurations for Category, Tag, Article, and CodeSnippet models in the articles app

--- backend/apps/articles/admin.py
+++ backend/apps/articles/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Tag, Article, ArticleTag, CodeSnippet
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'author', 'is_published', 'created_at']
+    list_filter = ['category', 'is_published']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['is_published']
+    raw_id_fields = ['author']
+
+
+@admin.register(CodeSnippet)
+class CodeSnippetAdmin(admin.ModelAdmin):
+    list_display = ['article', 'language', 'order']
+    list_filter = ['language']
+
+
+admin.site.register(ArticleTag)
